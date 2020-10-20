811427a6979f9a339ee0e63db0cd588e64fe2886
bertil291utn <bertilt4@gmail.com>
2020-10-20 14:33:37 -0500

    Add authorization token test

--- spec/models/user_rspec.rb
+++ spec/models/user_rspec.rb
@@ -1,7 +1,9 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  subject { create :user }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:email) }
 end
