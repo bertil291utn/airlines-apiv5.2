634cf48d703587df8c6181e9fa57b38314261ee8
bertil291utn <bertilt4@gmail.com>
2020-10-20 16:42:22 -0500

    Add  validate email format test

--- spec/models/user_rspec.rb
+++ spec/models/user_rspec.rb
@@ -6,4 +6,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_uniqueness_of(:email) }
+  it { should allow_value('myemail@email.com').for(:email) }
+  it { should_not allow_value('myemailemail.com').for(:email) }
 end
