17fc9e87116fca34f1d93124a30b91b8b9e9a064
bertil291utn <bertilt4@gmail.com>
2020-10-21 12:10:30 -0500

    Add new user method and tests

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,6 +1,15 @@
 class V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     render json: @user
   end
@@ -9,5 +18,9 @@ class V1::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe V1::UsersController, type: :controller do
-  let(:usuario) { create :user }
   describe 'GET#show/:id' do
+    let(:usuario) { create :user }
     subject { get :show, params: { id: usuario.id } }
     it { should have_http_status(:success) }
 
@@ -11,5 +11,11 @@ RSpec.describe V1::UsersController, type: :controller do
       json_response = JSON.parse(response.body)
       expect(json_response['email']).to match(usuario.email)
     end
+  end
+
+  describe 'create new user' do
+    subject(:usuario) { create :user }
+    subject { post :create, params: { name: usuario.name, email: usuario.email, password: usuario.password_digest } }
+    it { expect(response).to have_http_status(:success) }
   end
 end
