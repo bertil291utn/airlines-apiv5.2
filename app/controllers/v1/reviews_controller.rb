0ebf70a769a32632f8001944f572770e589f5b00
bertil291utn <bertilt4@gmail.com>
2020-10-22 17:38:21 -0500

    List all reviews - Add controller index - Add  tests

--- app/controllers/v1/reviews_controller.rb
+++ app/controllers/v1/reviews_controller.rb
@@ -1,6 +1,11 @@
 class V1::ReviewsController < ApplicationController
   before_action :set_review, only: %i[show update destroy]
 
+  def index
+    @review = Review.all
+    render json: @review, status: :ok
+  end
+
   def show
     render json: @user
   end
--- spec/controllers/v1/reviews_controller_spec.rb
+++ spec/controllers/v1/reviews_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe V1::ReviewsController, type: :controller do
-  describe 'GET #show' do
-    let(:airline) { create :airline }
-    let(:review) { create :review }
+  let(:airline) { create :airline }
+  let(:review) { create :review }
+
+  describe 'GET #show/:id' do
     subject { get :show, params: { id: review.id } }
+    it { should have_http_status(:success) }
+  end
+
+  describe 'GET #index' do
+    subject { get :index }
     it { should have_http_status(:success) }
   end
 end
