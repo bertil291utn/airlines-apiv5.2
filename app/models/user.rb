d167325a0c9152dbcc2affa7f9ffdbb11a1c083c
bertil291utn <bertilt4@gmail.com>
2020-10-26 10:27:12 -0500

    Add serializer user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,4 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates_presence_of :name, :email, :password_digest
-
-  def as_json(_ = {})
-    super(except: %i[created_at updated_at password_digest])
-  end
 end
